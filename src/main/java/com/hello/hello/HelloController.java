a4e7366040c4f93c0b8ac4d3117d266f246003c9
Leon Levy <leon.s.levy@gmail.com>
2018-06-20 12:17:04 -0700

    changed name of api function name

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
 
     @RequestMapping("/api")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> json() {
+    public Map<String, String> api() {
         HashMap<String, String> map = new HashMap<>();
         map.put("greeting", greeting);
         return map;
